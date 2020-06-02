b9ee1625fc763c9c055619a0a30d16154675e022
q2044757581 <32102666+q2044757581@users.noreply.github.com>
2020-06-02 11:54:06 +0800

    NN

--- NN/NN.py
+++ NN/NN.py
@@ -2,6 +2,7 @@ import torch
 import torch.nn as nn
 import numpy as np
 import torch.nn.functional as F
+import torch.optim as opt
 
 
 class Net(nn.Module):
@@ -11,9 +12,31 @@ class Net(nn.Module):
         self.h2 = nn.Linear(2, 1)
 
     def forward(self, x):
-        x = self.h1(x)
-        x = self.h2(x)
-        return F.sigmoid(x)
+        x = F.sigmoid(self.h1(x))
+        x = F.sigmoid(self.h2(x))
+        return x
 
 
-X = torch.Tensor()
+net = Net()
+print(net)
+params = list(net.parameters())
+print(len(params))
+X = np.array([[-1, -1], [-1, 1], [1, -1], [1, 1]])
+X = torch.from_numpy(X).float()
+Y = np.array([1, 0, 0, 1])
+Y = torch.from_numpy(Y)
+Y = Y.view(-1, 1).float()
+optimizer = opt.Adam(net.parameters(), lr=0.001)
+for i in range(10000):
+    # 清除梯度
+    optimizer.zero_grad()
+    # 计算输出
+    y_hat = net(X)
+    # 计算loss
+    loss = F.mse_loss(y_hat, Y)
+    print(i, loss)
+    # loss反向传播
+    loss.backward()
+    optimizer.step()
+
+print(net(X))
